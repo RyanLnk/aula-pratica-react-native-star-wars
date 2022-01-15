a47a6a5fded6d365d100224a1974feed68d463e8
Ryan Silva <rdsp1@aluno.ifnmg.edu.br>
2022-01-15 20:50:05 -0300

    Aula parte 8

--- src/screens/PeopleListScreen.js
+++ src/screens/PeopleListScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import api from '../services/api';
 import tw from 'twrnc';
@@ -9,13 +9,24 @@ import PersonItem from '../components/PersonItem';
 
 const PeopleListScreen = () => {
   const [people, setPeople] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [total, setTotal] = useState(0);
+
+  async function loadPeople() {
+    if (people.length === 0 || people.length < total) {
+      setLoading(true);
+
+      const response = await api.get(`/people/?page=${page}`);
+      setPeople([...people, ...response.data.results]);
+      setPage(page + 1);
+      setTotal(response.data.count);
+
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
-    async function loadPeople() {
-      const response = await api.get('/people');
-      setPeople(response.data.results);
-    }
-
     loadPeople();
   }, []);
 
@@ -31,7 +42,12 @@ const PeopleListScreen = () => {
         data={people}
         renderItem={({ item }) => <PersonItem person={item} />}
         keyExtractor={(item) => item.name}
+        onEndReached={loadPeople}
+        onEndReachedThreshold={0.1}
+        showsVerticalScrollIndicator={false}
       />
+
+      {loading && <ActivityIndicator size={25} color="#9CA3AF" />}
     </SafeAreaView>
   );
 };
